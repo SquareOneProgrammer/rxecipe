d780a131aaf22ccdc7f926ef7d14565b231c3867
SquareOneProgrammer <kitchen2code@gmail.com>
2023-09-12 13:54:34 -0400

    Initial commit

---
+++ src/main/java/com/rxecipe/RxecipeApplication.java
@@ -0,0 +1,13 @@
+package com.rxecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rxecipe/RxecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rxecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RxecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
